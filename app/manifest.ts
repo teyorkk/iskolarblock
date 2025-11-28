511c6f0dce3254bba771304935d6ce8fa20c71bd
teyorkk <theomacatienza@gmail.com>
2025-11-28 20:58:47 +0800

    change logo

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,15 +12,15 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#f97316",
     icons: [
       {
-        src: "/sk-logo.png",
+        src: "/iskolarblock.svg",
         sizes: "192x192",
-        type: "image/png",
+        type: "image/svg+xml",
         purpose: "any",
       },
       {
-        src: "/sk-logo.png",
+        src: "/iskolarblock.svg",
         sizes: "512x512",
-        type: "image/png",
+        type: "image/svg+xml",
         purpose: "any",
       },
       {
