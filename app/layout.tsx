f6de985db786d602c8d7b2e4af5e2066babc5619
teyorkk <theomacatienza@gmail.com>
2025-12-04 23:22:01 +0800

    added analytics by vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { SessionProvider } from "@/components/session-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <SessionProvider>
           <SpeedInsights />
+          <Analytics />
           {children}
         </SessionProvider>
       </body>
